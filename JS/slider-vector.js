64db427ddc12dc1d2fb8e109b10b15b6db5a0a1d
TomasRodriguez30 <rodrigueztomas411@gmail.com>
2023-11-14 20:03:39 -0300

    Fix estilos  Sobre Nosotros

--- JS/slider-vector.js
+++ JS/slider-vector.js
@@ -1,11 +1,11 @@
-const slides = document.querySelectorAll('.slide');
+const slides = document.querySelectorAll('.slide-sb');
 let currentIndex = 0;
 const slideCount = slides.length;
 
 function nextSlide() {
-  slides[currentIndex].classList.remove('active');
+  slides[currentIndex].classList.remove('active-sb');
   currentIndex = (currentIndex + 1) % slideCount;
-  slides[currentIndex].classList.add('active');
+  slides[currentIndex].classList.add('active-sb');
 }
 
 // Cambiar automáticamente de diapositiva cada 5 segundos (ajusta el tiempo según tus preferencias)
